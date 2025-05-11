abacd3731be1163648ec38eb7dde3705e1885e38
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-11 16:00:00 +0200

    Update DatabaseSeeder to create agents with queues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Agent;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(QueueSeeder::class);
+        // $this->call(QueueSeeder::class);
+       Agent::factory()->withQueues()->count(2)->create();
+
+        // $this->call(AgentAvailabilitySeeder::class);
     }
 }
