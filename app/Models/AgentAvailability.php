04c2155e6b8154dd013b38e4f07f0bddc78b2dc9
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-11 16:56:48 +0200

    Add method to retrieve agent schedules for a specified date range

--- app/Models/AgentAvailability.php
+++ app/Models/AgentAvailability.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class AgentAvailability extends Model
 {
     use HasFactory;
+
+    public static function getAllAgentScheduleForDateRange($startDate, $endDate)
+    {
+        return self::whereBetween('date', [$startDate, $endDate])->get();
+    }
 }
