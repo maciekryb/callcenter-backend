75e936cd38c9bc467b2c2cb3dfdf769b09e51ab1
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-11 16:55:08 +0200

    Add route for fetching all agents' schedules

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\QueueController;
+use App\Http\Controllers\AgentAvailabilityController; // Dodaj import kontrolera
 
 Route::post('agents', [AgentController::class, 'create']);  // Tworzymy agenta
 
 Route::get('queues', [QueueController::class, 'getQueues']); // Pobieramy wszystkie kolejki
+
+
+Route::get('agent/schedule', [AgentAvailabilityController::class, 'getAllAgentsSchedule']); // Pobieramy grafik dla wszystkich agentów
