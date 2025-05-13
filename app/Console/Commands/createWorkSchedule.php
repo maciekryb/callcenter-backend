6daa6a3624924133b5cfc0fd8b52ca0778bdd1d4
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-13 23:07:25 +0200

    Add createWorkSchedule command to automate work schedule creation

---
+++ app/Console/Commands/createWorkSchedule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\WorkSchedule;
+use App\Services\WorkScheduleService;
+use Illuminate\Console\Command;
+
+class createWorkSchedule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-work-schedule';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Creating work schedule...');
+
+        // Call the WorkScheduleService to create the work schedule
+        $date = now()->startOfWeek();
+
+        $this->info('Creating work schedule for date: ' . $date);
+        WorkScheduleService::createWorkSchedule($date);
+
+        $this->info('Work schedule created successfully.');
+    }
+}
