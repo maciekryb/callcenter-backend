da44a7b7439fcb863c9f7ba30af699cdb56b0606
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-13 22:48:25 +0200

    Add WorkSchedule model and migration for work schedules with availability status

--- app/Models/WorkSchedule.php
+++ app/Models/WorkSchedule.php
@@ -9,5 +9,11 @@ class WorkSchedule extends Model
 {
     use HasFactory;
 
+    const AVAILABILITY_FULL_DAY = "full_day";
+    const AVAILABILITY_PARTIAL_DAY = "partial_day";
 
+    const validAvailabilityStatus = [
+        self::AVAILABILITY_FULL_DAY,
+        self::AVAILABILITY_PARTIAL_DAY,
+    ];
 }
---
+++ database/migrations/2025_05_13_014414_create_work_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\WorkSchedule;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('queue_id')->constrained('queues')->onDelete('cascade');
+            $table->foreignId('agent_id')->constrained('agents')->onDelete('cascade');
+            $table->date('date');
+            $table->enum('work_status', WorkSchedule::validAvailabilityStatus);
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_schedules');
+    }
+};
