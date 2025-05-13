b9a30d0128e4d0c535b7d0284313d90fd0ab25d8
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-13 21:34:48 +0200

    Update API routes to correct agent schedule endpoint and add workload prediction route

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgentController;
 use App\Http\Controllers\QueueController;
 use App\Http\Controllers\AgentAvailabilityController; // Dodaj import kontrolera
+use App\Http\Controllers\WorkLoadPredictionController;
 
 Route::post('agents', [AgentController::class, 'create']);  // Tworzymy agenta
 
 Route::get('queues', [QueueController::class, 'getQueues']); // Pobieramy wszystkie kolejki
 
-Route::get('agent/schedule', [AgentAvailabilityController::class, 'getAllAgentsSchedule']); // Pobieramy grafik dla wszystkich agentów
-
 Route::get('queue/{id}/agents-schedule', [AgentAvailabilityController::class, 'getAgentsScheduleByQueueId']); // Pobieramy grafik agentów dla danej kolejki po id
+
+Route::get('queue/{id}/work-load', [WorkLoadPredictionController::class, 'getWorkLoadPrediction']); // Pobieramy prognozę obciążenia dla danej kolejki po id
