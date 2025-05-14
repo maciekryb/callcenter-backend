acffc22942f0c73584f9f8f105e150ea4093114b
Maciej Rybak <maciek.rybak95@gmail.com>
2025-05-14 19:57:32 +0200

    Add route for fetching work schedule by queue ID in api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AgentController;
 use App\Http\Controllers\QueueController;
 use App\Http\Controllers\AgentAvailabilityController; // Dodaj import kontrolera
 use App\Http\Controllers\WorkLoadPredictionController;
+use App\Http\Controllers\WorkScheduleController;
 
 Route::post('agents', [AgentController::class, 'create']);  // Tworzymy agenta
 
@@ -13,3 +14,5 @@ Route::get('queues', [QueueController::class, 'getQueues']); // Pobieramy wszyst
 Route::get('queue/{id}/agents-schedule', [AgentAvailabilityController::class, 'getAgentsScheduleByQueueId']); // Pobieramy grafik agentów dla danej kolejki po id
 
 Route::get('queue/{id}/work-load', [WorkLoadPredictionController::class, 'getWorkLoadPredictionByQueueId']); // Pobieramy prognozę obciążenia dla danej kolejki po id
+
+Route::get('work-schedule/queue/{id}', [WorkScheduleController::class, 'getWorkScheduleByQueueId']); // Pobieramy grafik agentów dla danej kolejki po id
